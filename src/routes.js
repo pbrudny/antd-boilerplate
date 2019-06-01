dc0e490eed228f1e35ba9c5a2128e4c546b2e3b0
Piotr Brudny <pbrudny@gmail.com>
2019-06-01 18:22:00 +0100

    add green page

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from "react-router-dom";
 import JobsContainer from "./jobs/JobsContainer";
 import JobDetailsContainer from "./jobs/JobDetailsContainer";
 import NotFound from "./pages/NotFound";
+import AntContainer from "./AntContainer";
 
 const Routes = () => (
   <main>
     <Switch>
-      <Route exact path="/" component={JobsContainer} />
+      <Route exact path="/" component={AntContainer} />
+      <Route exact path="/jobs" component={JobsContainer} />
       <Route exact path="/jobs/:number" component={JobDetailsContainer} />
       <Route exact path="/:city/:technology" component={JobsContainer} />
       <Route exact path="/*" component={NotFound} />
