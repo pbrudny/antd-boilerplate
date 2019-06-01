216224def6bec55d84a646aa6c24d16a8c34bf04
Piotr Brudny <pbrudny@gmail.com>
2019-06-01 18:24:17 +0100

    comment out routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import JobsContainer from "./jobs/JobsContainer";
-import JobDetailsContainer from "./jobs/JobDetailsContainer";
+// import JobsContainer from "./jobs/JobsContainer";
+// import JobDetailsContainer from "./jobs/JobDetailsContainer";
 import NotFound from "./pages/NotFound";
 import AntContainer from "./AntContainer";
 
@@ -9,9 +9,9 @@ const Routes = () => (
   <main>
     <Switch>
       <Route exact path="/" component={AntContainer} />
-      <Route exact path="/jobs" component={JobsContainer} />
-      <Route exact path="/jobs/:number" component={JobDetailsContainer} />
-      <Route exact path="/:city/:technology" component={JobsContainer} />
+      {/*<Route exact path="/jobs" component={JobsContainer} />*/}
+      {/*<Route exact path="/jobs/:number" component={JobDetailsContainer} />*/}
+      {/*<Route exact path="/:city/:technology" component={JobsContainer} />*/}
       <Route exact path="/*" component={NotFound} />
     </Switch>
   </main>
